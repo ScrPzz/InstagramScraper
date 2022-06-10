c7e5f176c73203492e782bded8c29afd29374d56
ScrPzz <alessandrotogni1985@gmail.com>
2022-06-10 16:11:57 +0200

    fixed imports

--- scrape_comments.py
+++ scrape_comments.py
@@ -1,17 +1,13 @@
 from src.comments_scraper import CommentsScraper
-import json
-import pandas as pd
 
-if __name__=='__main__':
+if __name__ == "__main__":
 
-    scraper=CommentsScraper()
+    scraper = CommentsScraper()
 
-    Driver, proxy,  args = scraper.setup()
+    Driver, proxy, args = scraper.setup()
 
-    raw_data =scraper.scrape(driver=Driver, args=args, proxy=proxy, save_raw_data=True)
+    raw_data = scraper.scrape(driver=Driver, args=args, proxy=proxy, save_raw_data=True)
 
     Driver.quit()
-    
-    _=scraper.parse_and_save_data(raw_data=raw_data, args=args)
 
-    
+    _ = scraper.parse_and_save_data(raw_data=raw_data, args=args)
--- scrape_likes.py
+++ scrape_likes.py
@@ -1,13 +1,9 @@
 from src.likes_scraper import LikesScraper
-import json
-import pandas as pd
 
-if __name__=='__main__':
+if __name__ == "__main__":
 
-    scraper=LikesScraper()
+    scraper = LikesScraper()
 
     Driver, args = scraper.setup()
 
     scraper.scrape(driver=Driver, args=args)
-    
-    
